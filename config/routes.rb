75d43701cb735a25799fa3a58b8479c3d50a70e0
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-13 15:05:22 +0000

    in_cording

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'tests/top'
+  root to: "tests#top"
+  # get 'tests/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
